b5c24e70ed7413a0b73db3203af40fb02b5b67b8
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-01-28 01:34:44 +0200

    Update tune_view.dart

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -1,44 +1,53 @@
+import 'package:audio/Items/audioItem.dart';
+import 'package:audio/model/tune.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:music_notes_player_app_setup/models/tune_model.dart';
-import 'package:music_notes_player_app_setup/widgets/tune_item.dart';
 
-class TuneView extends StatelessWidget {
-  const TuneView({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,});
 
-  final List<TuneModel> tunes = const [
-    TuneModel(color: Color(0xffF44336), sound: 'note1.wav'),
-    TuneModel(color: Color(0xffF89800), sound: 'note2.wav'),
-    TuneModel(color: Color(0xffFEEB3B), sound: 'note3.wav'),
-    TuneModel(color: Color(0xff4CAF50), sound: 'note4.wav'),
-    TuneModel(color: Color(0xff2F9688), sound: 'note5.wav'),
-    TuneModel(color: Color(0xff2896F3), sound: 'note6.wav'),
-    TuneModel(color: Color(0xff9C27B0), sound: 'note7.wav'),
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Tune>tunColor=[
+   Tune(color: Colors.red, sound: 'assets/note1.wav'),
+    Tune(color: Colors.black, sound: 'assets/note2.wav'),
+    Tune(color: Colors.white, sound: 'assets/note3.wav'),
+    Tune(color: Colors.grey, sound: 'assets/note4.wav'),
+    Tune(color: Colors.yellow, sound: 'assets/note5.wav'),
+    Tune(color:Colors.green, sound: 'assets/note6.wav'),
+    Tune(color:Colors.blue, sound: 'assets/note7.wav'),
+    Tune(color: Colors.orange, sound: 'assets/note1.wav'),
+
+
+
   ];
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Flutter Tune'),
-        backgroundColor: const Color(0xff253238),
-        elevation: 0,
+        title :Text('Flutter Tune',style: TextStyle(color: Colors.white,fontSize: 20),),
+        backgroundColor: Color(0XFF253238),
       ),
-      body: Column(
-        children: tunes
-            .map(
-              (e) => TuneItem(tune: e),
-            )
-            .toList(),
+
+      body:Column(
+
+        children: tunColor.map((e)=>audio(tn: e)).toList(),
+
+
+
+
+
       ),
     );
   }
 
-  // List<TuneItem> getTuneItems() {
-  //   List<TuneItem> items = [];
 
-  //   for (var color in tuneColors) {
-  //     items.add(TuneItem(color: color));
-  //   }
-  //   return items;
-  // }
+
 }
